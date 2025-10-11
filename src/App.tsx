3ce5d619ad218fecc45424cabe2ca7411203b6df
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-10-11 15:37:40 -0400

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-3xl font-bold underline">Hello You</p>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
