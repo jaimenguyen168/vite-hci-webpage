a823b53aa8bba4ee5f81ca8531a2ce09a1d6b0c3
seungwoo900 <danielee982@gmail.com>
2025-10-16 01:53:18 -0400

    add job outcomes and opprtunities on courses page

--- src/pages/courses/ui/views/Courses.tsx
+++ src/pages/courses/ui/views/Courses.tsx
@@ -2,25 +2,35 @@ import coursesContent from "../../content/courses.json";
 import outcomesContent from "../../content/outcomes.json";
 import opportunitiesContent from "../../content/opportunities.json";
 import CoursesList from "./CourseList";
-// import JobOutcomes from "./JobOutcomes";
-// import Opportunities from "./Opportunities";
+import JobOutcomes from "./JobOutcomes";
+import Opportunities from "./Opportunities";
 
 const CoursesPage = () => {
   return (
-    <section className="space-y-8">
+    <section className="space-y-12 pb-30">
       {/* Intro text with red border */}
-      <div className="border-r-16 pr-6 pl-18" style={{ borderColor: '#AA2C45' }}>
-        <p className="text-xl md:text-2xl font-roboto leading-relaxed text-right">
+      <div className="border-r-16 pr-6" style={{ borderColor: '#AA2C45' }}>
+        <h2 className="text-xl md:text-[30px] font-outfit leading-relaxed text-right">
           The HCI Lab opens pathways for students to explore careers in technology, design, and
           research. Through hands-on projects, mentorship, and industry connections, students
           gain the skills and experiences to turn curiosity into impact.
-        </p>
+        </h2>
       </div>
 
       {/* Cards */}
       <CoursesList content={coursesContent} />
-      {/* <JobOutcomes content={outcomesContent} />
-      <Opportunities content={opportunitiesContent} /> */}
+
+      <JobOutcomes content={outcomesContent} />
+
+      {/* Center text with red border */}
+      <div className="border-l-16 pr-10 pl-6" style={{ borderColor: '#AA2C45' }}>
+        <h2 className="text-xl md:text-[30px] font-outfit leading-relaxed text-left">
+          Our lab strives to advance the tech, design, and research communities here in Philadelphia.
+          While we support many students directly, we're often at or nearing capacity for mentorship.
+        </h2>
+      </div>
+
+      <Opportunities content={opportunitiesContent} />
     </section>
   );
 };
